8f66d4b1bd82167680c039a49fd444fc6f4c4a5a
Alucard <jesusarmand20@gmail.com>
2022-11-03 15:56:27 -0500

    Se agrega el Proyecto

---
+++ 24AMPag/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using _24AMPag.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _24AMPag.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+            { }
+            public DbSet<Usuario> Usuarios { get; set; }
+
+            public DbSet<Rol> Roles { get; set; }
+            
+    }
+}
---
+++ 24AMPag/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using _24AMPag.Context;
+using _24AMPag.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _24AMPag.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var response = await _context.Usuarios.Include(z => z.Rol).ToListAsync();
+            return View(response);
+        }
+
+        [HttpGet]
+        public IActionResult crear()
+        {
+            return View();
+            
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
